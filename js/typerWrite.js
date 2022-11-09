82724b9df66203f4704dc722ec60660347b2fbb2
FrankUrdaneta <6erozz9@gmail.com>
2022-11-09 12:53:26 -0300

    4 commit

--- js/typerWrite.js
+++ js/typerWrite.js
@@ -13,3 +13,32 @@ typewriter.typeString('Viene')
     .typeString('<strong>Estamos aqui</strong>')
     .pauseFor(2500)
     .start();
+
+
+    //agregamos autocompletar de la api google maps
+    var searchInput = 'search_input';
+
+$(document).ready(function () {
+    var autocomplete;
+    autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+        types: ['geocode'],
+    });
+        
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var near_place = autocomplete.getPlace();
+        document.getElementById('loc_lat').value = near_place.geometry.location.lat();
+        document.getElementById('loc_long').value = near_place.geometry.location.lng();
+                
+        document.getElementById('latitude_view').innerHTML = near_place.geometry.location.lat();
+        document.getElementById('longitude_view').innerHTML = near_place.geometry.location.lng();
+    });
+});
+
+//eliminamos latitud y longitud
+$(document).on('change', '#'+searchInput, function () {
+    document.getElementById('latitude_input').value = '';
+    document.getElementById('longitude_input').value = '';
+        
+    document.getElementById('latitude_view').innerHTML = '';
+    document.getElementById('longitude_view').innerHTML = '';
+});
