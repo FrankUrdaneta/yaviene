f1bae3a37bcf4525eb920e9bb66518001574be7a
FrankUrdaneta <6erozz9@gmail.com>
2022-11-26 12:29:30 -0300

    27committ

--- js/buscador_pasajes.js
+++ js/buscador_pasajes.js
@@ -7,12 +7,23 @@ let buscar=document.createElement('div')
 buscador.innerHTML=`<div class="forma mt-2 position-sticky">
 <form class="">
   <div class="row g-3">
-    
+  <div class="col-auto">
+  <select class="form-select idayVuelta" id="inputGroupSelect01">
+  <option selected>Ida y vuelta</option>
+  <option value="1">Ida</option>
+</select>
+</div>
+
+<div class="col-auto">
+  <select class="form-select pasajero" id="inputGroupSelect01">
+  <option >1 Pasajero</option>
+</select>
+</div>
   </div>
   
 </form>
 <form class="row g-3 mx-auto">
-  <div class="col-lg-2 col-sm-6">
+  <div class="col-lg-2 col-sm-6 cajasBuscador">
   <div class="row row g-3 origenPasaje">
   <div class="col-1 origenIcono"><label for="inputtext" class=""><i class="icon fa fa-dot-circle-o" style="color:#009ee2" aria-hidden="true"></i>   </label></div>
   <div class="col-7 origenInput" id="origenInput"><input type="text"  class="form-control inputOrigen" id="button" placeholder="Origen"><button class="btn btn-alert-danger invertirDestino"><i class="icon bi bi-arrow-left-right" style="color:white" aria-hidden="true"></i></button></div>
@@ -20,7 +31,7 @@ buscador.innerHTML=`<div class="forma mt-2 position-sticky">
   </div>
   </div>
   
-  <div class="col-lg-2 col-sm-6">
+  <div class="col-lg-2 col-sm-6 cajasBuscador">
   <div class="row origenPasaje">
   <div class="col-1 origenIcono">    <label for="inputtext" class=""><i class="icon fa fa-map-marker" style="color:#009ee2" aria-hidden="true"></i>  </label>
   </div>
@@ -28,15 +39,9 @@ buscador.innerHTML=`<div class="forma mt-2 position-sticky">
   </div>
   </div>
 
-  <div class="col-lg-2 col-sm-6">
-  <div class="row origenPasaje">
-  <div class="col-1 origenIcono"> <label for="inputtext" class=""></label><i class="icon fa fa-users" style="color:#009ee2" aria-hidden="true"></i> </label>
-  </div>
-  <div class="col-7 origenInput"><input type="number" class="form-control inputOrigen" min="1" id="input" placeholder="Pasajeros"></div>
-  </div>
-  </div>  
+  
     
-  <div class="col-lg-2 col-sm-6">
+  <div class="col-lg-2 col-sm-6 cajasBuscador">
   <div class="row origenPasaje">
   <div class="col-1 origenIcono"> <label for="inputtext" class=""> <i class="icon fa fa-calendar"  style="color:#009ee2" aria-hidden="true"></i></label>
   </div>
@@ -44,7 +49,7 @@ buscador.innerHTML=`<div class="forma mt-2 position-sticky">
   </div>
   </div>
 
-  <div class="col-lg-2 col-sm-6">
+  <div class="col-lg-2 col-sm-6 cajasBuscador">
   <div class="row origenPasaje">
   <div class="col-1 origenIcono"> <label for="inputtext" class=""> <i class="icon fa fa-calendar"  style="color:#009ee2" aria-hidden="true"></i></label>
   </div>
@@ -53,7 +58,7 @@ buscador.innerHTML=`<div class="forma mt-2 position-sticky">
   </div>
 
   <div class="col-lg-2 col-sm-6">
-   <button  id="buscar_pasajes"  class="btn ov-btn-grow-skew botonBuscarPasajes">Buscar</button>
+   <a href="buscador_pasajes.html"><button  id="buscar_pasajes"  class="btn ov-btn-grow-skew botonBuscarPasajes">Buscar</button></a>
   </div>
 </form>
 
