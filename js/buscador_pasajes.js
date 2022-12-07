59531719b801b59780fe3524006a538bf63a79ea
FrankUrdaneta <6erozz9@gmail.com>
2022-12-07 14:40:03 -0300

    modificacion boostrapCss indexhtml buscadorhtml

--- js/buscador_pasajes.js
+++ js/buscador_pasajes.js
@@ -32,13 +32,7 @@ function mostrarBarraBuscador() {
       </div>
       </div>
 
-      <div class="col-lg-2 col-sm-6 cajasBuscador">
-      <div class="row origenPasaje">
-      <div class="col-1 origenIcono"> <label for="inputtext" class=""> <i class="icon fa fa-calendar"  style="color:#009ee2" aria-hidden="true"></i></label>
-      </div>
-      <div class="col-7 origenInput"><input type="text"  class="form-control inputOrigen" id="staticEmail2" placeholder="Añadir Vuel..."></div>
-      </div>
-      </div>
+      
 
       <div class="col-lg-2 col-sm-6">
       <a  id="buscar_pasajes" class="btn ov-btn-grow-skew  ">Buscar</a>
