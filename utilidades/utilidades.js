a86d3f25c9139cd20b899f9a614fb47f182122fc
FrankUrdaneta <6erozz9@gmail.com>
2022-12-19 09:58:58 -0300

    funcional calendario y boton cancelar

--- utilidades/utilidades.js
+++ utilidades/utilidades.js
@@ -7,3 +7,26 @@ function getParameterByName(name, url=window.location.href) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+
+
+function anchoPantalla(){
+    document.documentElement.clientWidth;
+    //capturamos el ancho de la pantalla para decidir como estructurar la pagina
+}
+
+function capturarDatoGetSearch(){
+    const param=new URLSearchParams(window.location.search);
+    let q=param.get("fecha_salida")
+}
+
+
+function capturarFechaCalendario(){
+      let dia_semana=new Date().toLocaleDateString('es',{weekday:"long"})
+      let dia_number=parseInt( new Date().toLocaleDateString('es',{day:"numeric"}))
+      let mes_number=parseInt( new Date().toLocaleDateString('es',{month:"2-digit"}))
+      let ano_number=parseInt( new Date().toLocaleDateString('es',{year:"numeric"}))
+      let fecha_completa=(new Date().toLocaleDateString('es', {  year:"numeric", month:"2-digit", day:"numeric"}))
+      let fec=(new Date().toLocaleDateString('es', {  year:"numeric", month:"2-digit", dia_number}))
+
+}
