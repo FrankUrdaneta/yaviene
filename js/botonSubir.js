5f4fa33db58a4b140941ea4be3e088ee54b14065
FrankUrdaneta <6erozz9@gmail.com>
2022-12-19 22:57:46 -0300

    funcional en todol

--- js/botonSubir.js
+++ js/botonSubir.js
@@ -1,23 +1,23 @@
 // botonSubir
 
-document.getElementById("button-up").addEventListener('click',scrollUp);
+// document.getElementById("button-up").addEventListener('click',scrollUp);
 
-function scrollUp(){
-    let currentScroll= document.documentElement.scrollTop || document.body.scrollTop;
-    if(currentScroll>0){
-        window.requestAnimationFrame(scrollUp);
-        window.scrollTo(0,currentScroll-(currentScroll/25) )
-    }
-}
+// function scrollUp(){
+//     let currentScroll= document.documentElement.scrollTop || document.body.scrollTop;
+//     if(currentScroll>0){
+//         window.requestAnimationFrame(scrollUp);
+//         window.scrollTo(0,currentScroll-(currentScroll/25) )
+//     }
+// }
 
 
-buttonUp= document.getElementById("button-up");
+// buttonUp= document.getElementById("button-up");
 
-window.onscroll= function(){
-    let scroll= document.documentElement.scrollTop;
-    if(scroll>100){
-       buttonUp.style.transform= "scale(1)";
-    }else if(scroll<100){
-        buttonUp.style.transform= "scale(0)";
-    }
-}
+// window.onscroll= function(){
+//     let scroll= document.documentElement.scrollTop;
+//     if(scroll>100){
+//        buttonUp.style.transform= "scale(1)";
+//     }else if(scroll<100){
+//         buttonUp.style.transform= "scale(0)";
+//     }
+// }
